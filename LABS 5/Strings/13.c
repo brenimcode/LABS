48ae6ceeca19cf7030e4ddaeaebc37c57e75ce60
Breno Oliveira Cavalcante <127551374+brenimcode@users.noreply.github.com>
2023-05-04 18:32:18 -0300

    Update 13.c

--- LABS 5/Strings/13.c
+++ LABS 5/Strings/13.c
@@ -3,6 +3,23 @@
 #include <string.h>
 
 int main(){
-    char;
+    char string[100];
+    setbuf(stdin,NULL);
+    gets(string);
+    int tam = strlen(string);
+    int f = tam - 1;
+    for (int i = 0; i <= tam; i++)
+    {
+        if (string[i] == string[f])
+        {
+             f--;
+        }
+       else if(string[i] != string[f]){
+        printf("Nao eh palindromo.");
+        return 0;
+       }
+    }
+    printf("Palindromo.");
+    
     return 0;
 }
