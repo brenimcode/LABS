ebd8e290d20750e95842c24add25287189af57ca
Breno Oliveira Cavalcante <127551374+brenimcode@users.noreply.github.com>
2023-05-04 18:10:34 -0300

    Update 12.c

--- LABS 5/Strings/12.c
+++ LABS 5/Strings/12.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//codificação de strings
 
 int main(){
-    char;
+    char string[100];
+    setbuf(stdin,NULL);
+    gets(string);
+    int length = strlen(string);
+    for (int i = 0; i < length; i++)
+    {
+        if ((string[i] > 96 && string[i] < 123) || (string[i] > 65 && string[i] < 90) )
+        {
+            //Condicional para realizar a soma de 3 apenas em letras.
+            string[i] +=3;
+        }
+    }
+    printf("%s",string);
     return 0;
 }
