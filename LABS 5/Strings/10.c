a06054ecd4bc445011a50de26f46d2507d661619
Breno Oliveira Cavalcante <127551374+brenimcode@users.noreply.github.com>
2023-05-04 17:44:39 -0300

    Update 10.c

--- LABS 5/Strings/10.c
+++ LABS 5/Strings/10.c
@@ -3,6 +3,13 @@
 #include <string.h>
 
 int main(){
-    char;
+    char nome[20];
+    float valor;
+    setbuf(stdin,NULL);
+    gets(nome);
+    scanf("%f",&valor);
+    float desconto = valor*0.10;
+    float avista = valor*0.90;
+    printf("%s:\nValor total:%.2f\nValor desconto:%.2f\nValor a vista:%.2f\n",nome,valor,desconto,avista);
     return 0;
 }
