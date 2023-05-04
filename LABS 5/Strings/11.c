203cc1dde3c5dcc3676f1cb59b35bf4780602c94
Breno Oliveira Cavalcante <127551374+brenimcode@users.noreply.github.com>
2023-05-04 18:03:39 -0300

    Update 11.c

--- LABS 5/Strings/11.c
+++ LABS 5/Strings/11.c
@@ -3,6 +3,20 @@
 #include <string.h>
 
 int main(){
-    char;
+    char S[100];
+    int i,j;
+    setbuf(stdin,NULL);
+    fgets(S,10,stdin);
+    scanf("%i%i",&i,&j);
+    if (i < 0 || j<0)
+    {
+        printf("erro");
+        return 0;
+    }
+    while (i < j)
+    {
+        printf("%c",S[i]);
+        i++;
+    }
     return 0;
 }
