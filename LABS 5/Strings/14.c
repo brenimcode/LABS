b26dfeb547a679e5a03c2054b0759d000432c4f0
Breno Oliveira Cavalcante <127551374+brenimcode@users.noreply.github.com>
2023-05-04 18:29:25 -0300

    Update 14.c

--- LABS 5/Strings/14.c
+++ LABS 5/Strings/14.c
@@ -3,6 +3,14 @@
 #include <string.h>
 
 int main(){
-    char;
+    char str1[50],str2[50];
+    setbuf(stdin,NULL);
+    gets(str1);
+    int n;
+    scanf("%i",&n);
+    setbuf(stdin,NULL);
+    fgets(str2,n,stdin);
+    strcat(str1,str2);
+    printf("%s",str1);
     return 0;
 }
